fe6c1c4a4a363d113bbba5068f21fcb8ba18105a
eliasdevmin <eliasaraujo1222@outlook.com>
2024-04-21 20:02:17 -0300

    ajuste de codigo e melhorias, junto com avanço dos estudos

--- src/Principal.java
+++ src/Principal.java
@@ -1,17 +1,33 @@
-public class Principal {
-    public static void main(String[] args) {
-        Filme meuFilme = new Filme();
-        meuFilme.nome = "O poderoso Chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
-        meuFilme.exibeFichaTecnica();
-        meuFilme.avalia(8);
-        meuFilme.avalia(7);
-        meuFilme.avalia(10);
-        meuFilme.avalia(9);
-        System.out.println(meuFilme.somaDasAvalicoes);
-        System.out.println(meuFilme.totalDeAvalicoes);
-        System.out.println(meuFilme.mediaDasAvalia());
+public class Principal {
+
+    public static void main(String[] args) {
+        Filme favorito = new Filme();
+
+        favorito.setNome("The Matrix");
+        favorito.setAnoDeLancamento(1999);
+        favorito.setDuracaoEmMinutos(135);
+        favorito.setIncluidoNoPlano(true);
+
+        favorito.exibeFichaTecnica();
+        favorito.avalia(9);
+        favorito.avalia(8);
+        favorito.avalia(9);
+
+        System.out.println("Média de avaliações do filme: " +favorito.pegaMedia());
+
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTeporada(10);
+        lost.setMinutosPorEpisodios(180);
+        System.out.println("Você vali demora todo esse tempo para maratonar " + lost.getDuracaoEmMinutos());
+
     }
+
 }
---
+++ src/br/com/alura/screenmatch/modelos/Serie.java
@@ -0,0 +1,48 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Serie extends Titulo{
+    private  int temporadas;
+    private boolean ativa;
+    private int episodiosPorTeporada;
+    private int minutosPorEpisodios;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public int getEpisodiosPorTeporada() {
+        return episodiosPorTeporada;
+    }
+
+    public int getMinutosPorEpisodios() {
+        return minutosPorEpisodios;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public void setEpisodiosPorTeporada(int episodiosPorTeporada) {
+        this.episodiosPorTeporada = episodiosPorTeporada;
+    }
+
+    public void setMinutosPorEpisodios(int minutosPorEpisodios) {
+        this.minutosPorEpisodios = minutosPorEpisodios;
+    }
+
+    @Override
+    public int getDuracaoEmMinutos() {
+        return temporadas * episodiosPorTeporada * minutosPorEpisodios;
+    }
+}
+
+
+
